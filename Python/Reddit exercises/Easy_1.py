9d81fd24114ab1db9f7d89312fdeba275e48d328
leekeh <71131586+leekeh@users.noreply.github.com>
2020-09-28 13:37:25 +0200

    Create Easy_1.py

---
+++ Python/Reddit exercises/Easy_1.py
@@ -0,0 +1,4 @@
+name = input("Hi there! Welcome to my program. Firstly, what is your name? ")
+age = input ("Alright! Next, please tell me your age. ")
+username = input("Finally, choose your username: ")
+print ("Your name is {}, you are {} years old, and your username is {}".format(name,age,username))
