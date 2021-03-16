be7f7533a14ad344b0db384a30e650f3c12281bb
leekeh <71131586+leekeh@users.noreply.github.com>
2021-03-16 13:05:17 +0100

    Update HelloWorld.java

--- Java/Bootcamp/HelloWorld.java
+++ Java/Bootcamp/HelloWorld.java
@@ -3,12 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package nl.qquest.hello_world;
 
-/**
- *
- * @author l.beunders
- */
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello world\n hi");
