3c66926564e969f40c292517535d276865b9cf24
leekeh <71131586+leekeh@users.noreply.github.com>
2021-05-05 12:17:08 +0200

    Create 1-1.js

---
+++ JavaScript/10_Days_of_JS/1-1.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function getArea(length, width) {
+    let area;
+    area = length*width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter = (length*2) + (width*2)
+    return perimeter;
+}
