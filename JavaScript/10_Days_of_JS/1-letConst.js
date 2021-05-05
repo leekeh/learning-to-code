051c01f4e270c64857b86fb42ceecc05804bbc05
leekeh <71131586+leekeh@users.noreply.github.com>
2021-05-05 13:26:02 +0200

    Create 1-letConst.js

---
+++ JavaScript/10_Days_of_JS/1-letConst.js
@@ -0,0 +1,9 @@
+function main() {
+    const PI = Math.PI;
+    let r = readLine();
+
+    // Print the area of the circle:
+    console.log(PI*r**2)
+    // Print the perimeter of the circle:
+    console.log(2*PI*r)
+}
